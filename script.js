ec4953cbc4df0f376951f2d2b2863c83db8a3552
djatiasmara09-dot <djatiasmara09@gmail.com>
2026-01-08 13:45:15 +0700

    Add files via upload
    galery foto

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const gallery = document.getElementById("gallery");
 
+let images = JSON.parse(localStorage.getItem("galleryImages")) || [];
+
+// tampilkan foto saat halaman dibuka
+window.onload = function () {
+    images.forEach(src => addImageToGallery(src));
+};
+
 function addImage() {
     const input = document.getElementById("imageInput");
 
     if (!input.files || input.files.length === 0) {
-        alert("Pilih foto dulu ya!");
+        alert("Pilih foto dulu!");
         return;
     }
 
@@ -12,14 +19,20 @@ function addImage() {
     const reader = new FileReader();
 
     reader.onload = function(e) {
-        const img = document.createElement("img");
-        img.src = e.target.result;
-        img.style.width = "180px";
-        img.style.margin = "10px";
-        img.style.borderRadius = "10px";
-
-        gallery.appendChild(img);
+        images.push(e.target.result);
+        localStorage.setItem("galleryImages", JSON.stringify(images));
+        addImageToGallery(e.target.result);
     };
 
     reader.readAsDataURL(file);
+}
+
+// fungsi untuk menampilkan foto
+function addImageToGallery(src) {
+    const img = document.createElement("img");
+    img.src = src;
+    img.style.width = "180px";
+    img.style.margin = "10px";
+    img.style.borderRadius = "10px";
+    gallery.appendChild(img);
 }
